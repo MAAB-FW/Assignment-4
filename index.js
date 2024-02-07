06ae937d5a109773cb79cac4e932aec85cff5693
maabfw <maabfw@proton.me>
2024-02-08 00:01:00 +0600

    5th and all problem solved

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// console.log(calculateMoney(10));
-// console.log(calculateMoney(1055));
-// console.log(calculateMoney(93));
-// console.log(calculateMoney(-130));
+console.log(calculateMoney(10));
+console.log(calculateMoney(1055));
+console.log(calculateMoney(93));
+console.log(calculateMoney(-130));
 
 function calculateMoney(ticketSale) {
     if (ticketSale < 0) {
@@ -15,12 +15,11 @@ function calculateMoney(ticketSale) {
 }
 
 //-------------------------------------
-
-// console.log(checkName('Salman'));
-// console.log(checkName('RAFEE'));
-// console.log(checkName('Jhankar'));
-// console.log(checkName(199));
-// console.log(checkName(["Rashed"]));
+console.log(checkName('Salman'));
+console.log(checkName('RAFEE'));
+console.log(checkName('Jhankar'));
+console.log(checkName(199));
+console.log(checkName(["Rashed"]));
 
 function checkName(name) {
     if (typeof name !== 'string') {
@@ -36,11 +35,10 @@ function checkName(name) {
 }
 
 // ---------------------------
-
-// console.log(deleteInvalids([1, null, undefined, 18, -19, NaN, "12", [1, 2], { ob: "lala" }]));
-// console.log(deleteInvalids(["1", { num: 2 }, NaN]));
-// console.log(deleteInvalids([1, 2, -3]));
-// console.log(deleteInvalids({ num: [1, 2, 3] }));
+console.log(deleteInvalids([1, null, undefined, 18, -19, NaN, "12", [1, 2], { ob: "lala" }]));
+console.log(deleteInvalids(["1", { num: 2 }, NaN]));
+console.log(deleteInvalids([1, 2, -3]));
+console.log(deleteInvalids({ num: [1, 2, 3] }));
 
 function deleteInvalids(array) {
     if (Array.isArray(array) !== true) {
@@ -56,11 +54,10 @@ function deleteInvalids(array) {
 }
 
 // ----------------------------
-
-// console.log(password({ name: "kolimuddin", birthYear: 1999, siteName: "google" }));
-// console.log(password({ name: "rahat", birthYear: 2002, siteName: "Facebook" }));
-// console.log(password({ name: "toky", birthYear: 200, siteName: "Facebook" }));
-// console.log(password({ name: "maisha", birthYear: 2002 }));
+console.log(password({ name: "kolimuddin", birthYear: 1999, siteName: "google" }));
+console.log(password({ name: "rahat", birthYear: 2002, siteName: "Facebook" }));
+console.log(password({ name: "toky", birthYear: 200, siteName: "Facebook" }));
+console.log(password({ name: "maisha", birthYear: 2002 }));
 
 function password(obj) {
     if (Object.keys(obj).length !== 3 || Object.values(obj)[1] > 9999 || Object.values(obj)[1] < 1000) {
@@ -73,3 +70,36 @@ function password(obj) {
     return webName + '#' + name + '@' + birthDate
 }
 
+// --------------------------
+console.log(monthlySavings([1000, 2000, 3000], 5400));
+console.log(monthlySavings([1000, 2000, 2500], 5000));
+console.log(monthlySavings([900, 2700, 3400], 10000));
+console.log(monthlySavings(100, [900, 2700, 3400]));
+
+function monthlySavings(arr, livingCost) {
+    if (Array.isArray(arr) !== true || typeof livingCost !== "number") {
+        return "invalid input"
+    }
+    // const tax= 
+    let earningArr = []
+    for (const taka of arr) {
+        if (taka >= 3000) {
+            let paidTax = taka - (taka * 20 / 100);
+            earningArr.push(paidTax)
+        }
+        if (taka < 3000) {
+            earningArr.push(taka)
+        }
+    }
+
+    let earning = 0;
+    for (const num of earningArr) {
+        earning += num
+    }
+    const savings = earning - livingCost;
+    if (savings >= 0) {
+        return savings
+    } else {
+        return "earn more"
+    }
+}
