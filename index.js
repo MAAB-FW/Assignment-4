422b4df00d8eebba76d468912a40d3aaa83ff7df
maabfw <maabfw@proton.me>
2024-02-07 21:17:39 +0600

    2nd problem solved

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-console.log(calculateMoney(10));
-console.log(calculateMoney(1055));
-console.log(calculateMoney(93));
-console.log(calculateMoney(-130));
+// console.log(calculateMoney(10));
+// console.log(calculateMoney(1055));
+// console.log(calculateMoney(93));
+// console.log(calculateMoney(-130));
 
 function calculateMoney(ticketSale) {
     if (ticketSale < 0) {
@@ -16,7 +16,21 @@ function calculateMoney(ticketSale) {
 
 //-------------------------------------
 
+// console.log(checkName('Salman'));
+// console.log(checkName('RAFEE'));
+// console.log(checkName('Jhankar'));
+// console.log(checkName(199));
+// console.log(checkName(["Rashed"]));
 
 function checkName(name) {
-    
+    if (typeof name !== 'string') {
+        return 'invalid'
     }
+    const lastAlph = 'ayieouw'
+    const lowName = name.toLowerCase()
+    if (lastAlph.includes(lowName[lowName.length - 1])) {
+        return 'Good Name'
+    } else {
+        return 'Bad Name'
+    }
+}
